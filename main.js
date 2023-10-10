89b4b979c9bed2a614cc2d75413224e2cd5b6cdf
Walterkibet04 <134695229+Walterkibet04@users.noreply.github.com>
2023-10-10 12:44:35 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function toggle() {
+    const showele = document.getElementById('showhide');
+
+    if(showele.style.display == 'none') {
+        showele.style.display = 'block';
+    }
+
+    else{
+        showele.style.display ='none';
+    }
+}
